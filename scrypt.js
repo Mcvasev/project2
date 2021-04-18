d1c6d0279efb08db95a4536ed22b161e7ab80a4f
ILYA <69519778+Mcvasev@users.noreply.github.com>
2021-04-18 19:56:31 +0300

    18

--- scrypt.js
+++ scrypt.js
@@ -1,18 +1,18 @@
 let add = document.querySelector('.knopkaAdd');
-let del = document.querySelector('.deleteButton');
+let doArea = document.querySelector('.doArea');
 
-add.addEventListener('click', (event) => {
+add.addEventListener('click', () => {
     let newDo = document.createElement('li');
-    newDo.innerHTML = `<form class="formText"><input class="do" type="text"><button type="button" class="buttonDelete">x</button>`;
-    document.querySelector('menu').append(newDo);
+    newDo.innerHTML = `<li class="table"><input class="doArea" type="text"><button type="button" class="buttonDelete">x</button>`;
+    document.querySelector('.menu').append(newDo);
     delElementsDoList();
 });
 
 function delElementsDoList() {
     let buttonDel = document.querySelectorAll('.buttonDelete');
+    console.log(buttonDel);
     buttonDel.forEach((el) => {
         el.addEventListener('click', (event) => {
-            event.preventDefault();
             event.target.parentElement.remove();
         });
     });
@@ -20,16 +20,17 @@ function delElementsDoList() {
 
 let arr = [];
 let buttonSort = document.querySelector('.grayB');
-
 let flag = true;
 buttonSort.addEventListener('click', (event) => {
     buttonSort.classList.remove('grayB');
     buttonSort.classList.add('grayBsort');
+    
     if(flag === true){
-        let doArray = document.querySelectorAll('.do');
-        arr = Array.from(menuArray);
+        let doArray = document.querySelectorAll('.doArea');
+        arr = Array.from(doArray);
+        console.log(arr);
         arr.sort((a,b) => {
-            if(a.value > b.value) {
+            if (a.value > b.value) {
                 return 1;
             } else if (a.value < b.value) {
                 return -1;
@@ -44,16 +45,17 @@ buttonSort.addEventListener('click', (event) => {
         buttonSort.classList.remove('grayBsort');
         buttonSort.classList.add('grayB');
         let arr2 = [];
-        let do2 = document.querySelectorAll('.do');
+        let doArea2 = document.querySelectorAll('.doArea');
+        arr2 = Array.from(doArea2);
         arr2.sort((a,b) => {
-            if(a.value > b.value) {
+            if (a.value > b.value) {
                 return -1;
             } else if (a.value < b.value) {
                 return 1;
             } return 0;
         });
         let menu2 = document.querySelector('.menu');
-        menu2.forEach((el) => {
+        arr2.forEach((el) => {
             menu2.appendChild(el.parentElement);
         });
         flag = true;
